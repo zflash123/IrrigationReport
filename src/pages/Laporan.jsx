9beab2182e7667e69b20e6d26cd7a7e3497cd3cf
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-17 13:26:27 +0700

    Add map to Laporan page and move the LocateUser component to component folder

--- src/pages/Laporan.jsx
+++ src/pages/Laporan.jsx
@@ -1,17 +1,28 @@
+import {MapContainer, TileLayer} from 'react-leaflet'
 import TopNavBar from "../components/TopNavBar";
 import BottomBar from "../components/BottomBar";
+import LocateUser from '../components/LocateUser';
 
 export default function Laporan(){
-    return(
-        <div className="page">
-            <TopNavBar />
-            <div className="content">
-                <h1>Ini halaman Laporan</h1>
-                <h1>Ini halaman Laporan</h1>
-                <h1>Ini halaman Laporan</h1>
-                <h1>Ini halaman Laporan</h1>
-            </div>
-            <BottomBar />
-        </div>
-    );
+	return(
+		<div className="page">
+			<TopNavBar />
+			<div className="leaflet-container">
+				<MapContainer
+					className="full-height-map"
+					center={[-7.87074500384173, 112.52647830035404]}
+					zoom={18}
+					minZoom={18}
+					maxZoom={19}
+					maxBounds={[[-85.06, -180], [85.06, 180]]}
+					scrollWheelZoom={true}>
+					<TileLayer
+						url="http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga"
+					/>
+					<LocateUser />
+				</MapContainer>
+			</div>
+			<BottomBar />
+		</div>
+	);
 }
