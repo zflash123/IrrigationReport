f7a573a1bf7a5a0cbb2b4770d05d0dd6573fa252
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-24 10:40:47 +0700

    Make register page more responsive

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,14 @@
+import "./Register.css"
+
 export default function Register() {
   return(
     <div className="login-content">
       <h1>Register</h1>
-      <input type="text" name="email" id="email" placeholder="email" />
-      <input type="text" name="password" id="password" placeholder="password" />
-      <input type="text" name="nama" id="nama" placeholder="nama" />
-      <input type="text" name="alamat" id="alamat" placeholder="alamat" />
-      <button>Register</button>
+      <input type="text" name="email" className="form-input" id="register-email" placeholder="email" />
+      <input type="text" name="password" className="form-input" id="register-password" placeholder="password" />
+      <input type="text" name="nama" className="form-input" id="register-name" placeholder="nama" />
+      <input type="text" name="alamat" className="form-input" id="register-address" placeholder="alamat" />
+      <button className="register-button">Register</button>
     </div>
   );
 }
