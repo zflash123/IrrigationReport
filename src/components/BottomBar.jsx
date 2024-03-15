52c2db2296754c704e4a0aadedfc48614b67e552
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-03-15 13:36:15 +0700

    Replace "a" element with Link Component so there is no reload whenever the link is clicked, so the app will be faster

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,30 +1,32 @@
+import { Link } from "react-router-dom"
+
 export default function BottomBar({activeIcon}) {
   return(
     <nav className="navbar navbar-expand navbar-light bg-light">
       <ul className="navbar-nav nav-justified w-100">
         <li className="nav-item">
-          <a href="/laporkan" className="nav-link text-center">
-          <img src={activeIcon === "laporkan" ? "/img/map-icon-purple.png" : "/img/map-icon.png"} width={'35vh'}/>
-          <span className="small d-block"><b>Laporkan</b></span>
-          </a>
+          <Link to={"/laporkan"} className="nav-link text-center">
+            <img src={activeIcon === "laporkan" ? "/img/map-icon-purple.png" : "/img/map-icon.png"} width={'35vh'}/>
+            <span className="small d-block"><b>Laporkan</b></span>
+          </Link>
         </li>
         <li className="nav-item">
-          <a href="/riwayat" className="nav-link text-center">
-          <img src={activeIcon === "riwayat" ? "/img/riwayat-icon-purple.png" : "/img/riwayat-icon.png"} width={'35vh'} />
-          <span className="small d-block"><b>Riwayat</b></span>
-          </a>
+          <Link to={"/riwayat"} className="nav-link text-center">
+            <img src={activeIcon === "riwayat" ? "/img/riwayat-icon-purple.png" : "/img/riwayat-icon.png"} width={'35vh'} />
+            <span className="small d-block"><b>Riwayat</b></span>
+          </Link>
         </li>
         <li className="nav-item">
-          <a href="/laporan" className="nav-link text-center">
-          <img src={activeIcon === "laporan" ? "/img/maps-icon-purple.png" : "/img/maps-icon.png"} width={'35vh'} />
-          <span className="small d-block"><b>Laporan</b></span>
-          </a>
+          <Link to={"/laporan"} className="nav-link text-center">
+            <img src={activeIcon === "laporan" ? "/img/maps-icon-purple.png" : "/img/maps-icon.png"} width={'35vh'} />
+            <span className="small d-block"><b>Laporan</b></span>
+          </Link>
         </li>
         <li className="nav-item">
-          <a href="/profil" className="nav-link text-center">
-          <img src={activeIcon === "profil" ? "/img/profile-icon-purple.png" : "/img/profile-icon.png"} width={'35vh'} />
-          <span className="small d-block"><b>Profil</b></span>
-          </a>
+          <Link to={"/profil"} className="nav-link text-center">
+            <img src={activeIcon === "profil" ? "/img/profile-icon-purple.png" : "/img/profile-icon.png"} width={'35vh'} />
+            <span className="small d-block"><b>Profil</b></span>
+          </Link>
         </li>
       </ul>
     </nav>
