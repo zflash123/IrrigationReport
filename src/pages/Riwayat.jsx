0968801938c3723489af8719d8590d3351ff26e6
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-03-28 20:10:03 +0700

    get filter query param; add filter by status name; make user can do filter and search at the same time; add loading in each fetching; change border-width in dropdown-menu

--- src/pages/Riwayat.jsx
+++ src/pages/Riwayat.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { Cookies } from "react-cookie";
 import LoadingPopUp from "../components/LoadingPopUp";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 export default function Riwayat(){
   const [riwayats, setRiwayats] = useState([]);
@@ -13,10 +14,68 @@ export default function Riwayat(){
   const [searchParams, setSearchParams] = useSearchParams();
 
   const search = searchParams.get("search");
+  const filter = searchParams.get("filter");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = Array.from(e.target.elements)
+      .filter((input) => input.name)
+      .reduce((obj, input) => Object.assign(obj, { [input.name]: input.value }), {});
+    
+    changeParams("search", data.search);
+  }
+  function changeParams(param, value) {
+    if(search!=null && filter!=null && param==="search"){
+      setSearchParams({ filter: filter, search: value });
+    }else if(search!=null){
+      if(param==="search"){
+        setSearchParams({ search: value });
+      }else if(param==="filter"){
+        setSearchParams({ search: search, filter: value });
+      }
+    } else if(filter!=null){
+      if(param==="filter"){
+        setSearchParams({ filter: value });
+      }else if(param==="search"){
+        setSearchParams({ filter: filter, search: value });
+      }
+    } else if(param==="search"){
+      setSearchParams({ search: value });
+    } else if(param==="filter"){
+      setSearchParams({ filter: value });
+    }
+  }
   useEffect(() => {
     const cookies = new Cookies();
-    if(search===null) {
+    if(search!=null&&filter!=null){
+      fetch('http://127.0.0.1:8000/api/user-reports?' + new URLSearchParams({
+        search: `${search}`,
+        filter: `${filter}`
+      }), {
+        headers: {Authorization: 'Bearer '+cookies.get('user_session')}
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setRiwayats(data);
+          setIsLoading(false);
+        });
+    } else if(filter!=null){
+      setIsLoading(true);
+      fetch('http://127.0.0.1:8000/api/user-reports?' + new URLSearchParams({
+        filter: `${filter}`
+      }), {
+        headers: {Authorization: 'Bearer '+cookies.get('user_session')}
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setRiwayats(data);
+          setIsLoading(false);
+        });
+    }else if(search===null) {
       fetch('http://127.0.0.1:8000/api/user-reports', {
         headers: {Authorization: 'Bearer '+cookies.get('user_session')}
       })
@@ -41,7 +100,7 @@ export default function Riwayat(){
           setIsLoading(false);
         });
     }
-  }, [search]);
+  }, [search, filter]);
 
   let navigate = useNavigate();
 
@@ -55,15 +114,24 @@ export default function Riwayat(){
       <TopNavBar />
       <div className="content">
         <div className="search-container">
-          <form action="" method="get" className="search-form">
-            <input type="text" name="search" id="search-bar" placeholder="Cari riwayat" />
-            <button className="b-search">
+          <form onSubmit={handleSubmit} method="get" className="search-form">
+            <input type="text" name="search" id="search-bar" placeholder="Cari riwayat"/>
+            <button className="b-search" type="submit">
               <i className="bi bi-search"></i>
             </button>
           </form>
-          <button className="b-search">
-            <i className="bi bi-funnel"></i>
-          </button>
+          <div className="btn-group">
+            <button type="button" className="b-search btn" data-bs-toggle="dropdown" aria-expanded="false">
+              <i className="bi bi-funnel"></i>
+            </button>
+            <ul className="dropdown-menu">
+              <li className="dropdown-item" style={{backgroundColor: "#B57EFB", color: "white"}} href="#">Filter Status:</li>
+              <li><a className="dropdown-item" onClick={()=>changeParams("filter", "Dalam Proses")}>Dalam Proses</a></li>
+              <li><a className="dropdown-item" onClick={()=>changeParams("filter", "Ditindak Lanjuti")}>Ditindak Lanjuti</a></li>
+              <li><a className="dropdown-item" onClick={()=>changeParams("filter", "Selesai")}>Selesai</a></li>
+              <li><a className="dropdown-item" href="/riwayat">Hapus Filter</a></li>
+            </ul>
+          </div>
         </div>
         {riwayats.map((riwayat) => (
           <div className="riwayat-box" key={riwayat.id} onClick={() => toDetailPage(riwayat.id+"/0")}>
