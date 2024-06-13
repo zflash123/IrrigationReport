9997a3bd2a9e7a7fae0cf7181815efe02674080f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-06-13 16:38:14 +0700

    Add zoom control and remove zoomIn that make a bug

--- src/components/LocateUser.jsx
+++ src/components/LocateUser.jsx
@@ -3,9 +3,7 @@ import { useMap } from "react-leaflet";
 export default function LocateUser({changeCoordinate}) {
   const map = useMap();
   map.attributionControl.setPrefix('');
-  map.zoomControl.remove();
   map.locate({setView: true});
-  map.zoomIn();
 
   map.on('locationfound', function(e) {
     const latitude = e.latitude;
