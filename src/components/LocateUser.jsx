3ea4374bceed0f7e29fd011c5728b8bd963afe8c
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-11-08 12:56:23 +0700

    Make the "map.locate" function executed only once. Add a checker. If the location is error, show a toast with the error message.

--- src/components/LocateUser.jsx
+++ src/components/LocateUser.jsx
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
 import { useMap } from "react-leaflet";
+import { toast } from "react-toastify";
 
 export default function LocateUser({changeCoordinate}) {
   const map = useMap();
   map.attributionControl.setPrefix('');
-  map.locate({setView: true});
+  useEffect(() => {
+    map.locate({setView: true});
+  }, [])
 
   map.on('locationfound', function(e) {
     const latitude = e.latitude;
     const longitude = e.longitude;
     changeCoordinate(latitude, longitude);
   });
+  map.on('locationerror', function() {
+    toast.error("Mohon izinkan akses ke lokasi", {autoClose: 10000})
+  })
+  useEffect(() => {
+  }, [])
   return;
 }
