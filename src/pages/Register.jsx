a9de8153c0f274d59729c9befcb00847a7859e12
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-11-08 13:05:12 +0700

    Import the LoadingPopUp component. Create isLoading state. Create changeIsLoading function. Create checker inside return. If the isLoading is true show LoadingPopUp component. Send the changeIsLoading function to RegisterForm component.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,22 @@
 import "./Register.css"
 import RegisterForm from "../components/RegisterForm";
+import LoadingPopUp from "../components/LoadingPopUp";
+import { useState } from "react";
 
 export default function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function changeIsLoading(bool) {
+    setIsLoading(bool);
+  }
+
   return(
-    <div className="login-content">
-      <h1 className="register-title">Register</h1>
-      <RegisterForm />
-    </div>
+    <>
+      {isLoading ? <LoadingPopUp /> : <></>}
+      <div className="login-content">
+        <h1 className="register-title">Register</h1>
+        <RegisterForm changeIsLoading={changeIsLoading}/>
+      </div>
+    </>
   );
 }
