82bb987a5905511621e5d5851d5ca1e38898ab6b
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-11-13 10:39:05 +0700

    Add a userPoint circle and circle around the user point to the map.

--- src/components/LocateUser.jsx
+++ src/components/LocateUser.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import { toast } from "react-toastify";
+import L from 'leaflet';
 
 export default function LocateUser({changeCoordinate}) {
   const map = useMap();
@@ -13,6 +14,20 @@ export default function LocateUser({changeCoordinate}) {
     const latitude = e.latitude;
     const longitude = e.longitude;
     changeCoordinate(latitude, longitude);
+    var userPoint = new L.Circle([latitude, longitude], {
+      color: 'white',
+      fillColor: 'blue',
+      fillOpacity: 1,
+      radius: 2,
+    })
+    var circleAround = new L.Circle([latitude, longitude], {
+      stroke: false,
+      fillColor: '#00aaff',
+      fillOpacity: 0.35,
+      radius: 7,
+    })
+    circleAround.addTo(map);
+    userPoint.addTo(map);
   });
   map.on('locationerror', function() {
     toast.error("Mohon izinkan akses ke lokasi", {autoClose: 10000})
